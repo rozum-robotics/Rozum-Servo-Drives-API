6a826abff174833f2e1b234f7f5caa25d20150ab
Alesia Bulanok <alesia.bulanok@rozum.com>
2018-06-25 18:05:48 +0300

    Update control_servo_traj_1.c

--- c/tutorial/control_servo_traj_1.c
+++ c/tutorial/control_servo_traj_1.c
@@ -1,6 +1,25 @@
 /**
- * @brief Tutorial example of one servo PVT move
- * 
+ * @brief The tutorial describes how to set two PVT (point, velocity, time) points for one servo and
+ * to execute the resulting motion trajectory.<br>
+ * The general sequence of actions is as follows:
+ * 1. Open the USB port where your CAN-USB dongle is connected using the ::rr_init_interface function.
+ * 2. Initiate the servo for which you want to set a motion trajectory using the ::rr_init_servo function.
+ * 3. Call the ::rr_clear_points_all function to clear the existing motion queue.
+ * 4. Call the ::rr_add_motion_point to set the first PVT point, commanding the motor to move to the position of 100 degrees in 6 seconds.
+ * The function parameters will be as follows:
+ * servo: servo descriptor returned by the ::rr_init_servo function.
+ * position_deg: 100.0 degrees
+ * velocity_deg: 0.0 degrees
+ * time_ms: 6000 milliseconds
+ * When the point is added successfully to the motion queue, the function will return OK. When adding the point fails, the function returns an error warning and quits the program.
+ * 5. Call the ::rr_add_motion_point to set the first PVT point, commanding the motor to move to the position of -100 degrees in 6 seconds.
+ * The function parameters will be as follows:
+ * servo: servo descriptor returned by the ::rr_init_servo function
+ * position_deg: -100.0 degrees
+ * velocity_deg: 0.0 degrees
+ * time_ms: 6000 milliseconds
+ * 6. Call the ::rr_start_motion to execute the motion queue comprising the two points added in Step 4 and Step 5.
+ * 7. Call the ::rr_sleep_ms function to set a latency period of 14 seconds. Calling the function ensures that master Heartbeats will be still available and the servo will not stop.
  * @file control_servo_traj_1.c
  * @author Rozum
  * @date 2018-06-25
