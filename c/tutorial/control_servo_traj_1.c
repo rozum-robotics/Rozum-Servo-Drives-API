91a80e3d26aac7d7111b7f2f887c90bf1526ae9c
Alesia Bulanok <alesia.bulanok@rozum.com>
2018-06-28 16:16:17 +0300

    Update control_servo_traj_1.c

--- c/tutorial/control_servo_traj_1.c
+++ c/tutorial/control_servo_traj_1.c
@@ -15,13 +15,13 @@
  * <li>one PVT commanding the servo to move to the position of -100 degrees in 6,000 milliseconds</li></ul>
  * 
  * 1. Initialize the interface.
- * \snippet control_servo_traj_1.c Adding the interface
+ * \snippet control_servo_traj_1.c Adding the interface1
  * 
  * 2. Initialize the servo.
- * \snippet control_servo_traj_1.c Adding the servo
+ * \snippet control_servo_traj_1.c Adding the servo1
  * 
  * 3. Clear the motion queue.
- * \snippet control_servo_traj_1.c Clear points all
+ * \snippet control_servo_traj_1.c Clear points all1
  * 
  * <b> Adding PVT points to form a motion queue </b>
  * 
@@ -36,10 +36,10 @@
  * <b> Executing the resulting motion queue </b>
  * 
  * 7. Command the servo to move through the PVT points you added to the motion queue. Set the function parameter to 0 to get the servo moving without a delay.
- * \snippet control_servo_traj_1.c Start motion
+ * \snippet control_servo_traj_1.c Start motion1
  * 
  * 8. To ensure the program will not move on to execute another operation, set an idle period of 14,000 milliseconds.
- * \snippet control_servo_traj_1.c Sleep
+ * \snippet control_servo_traj_1.c Sleep1
  * 
  * <b> Complete tutorial code: </b>
  * \snippet control_servo_traj_1.c cccode 1
@@ -48,19 +48,19 @@
 int main(int argc, char *argv[])
 {
     //! [cccode 1] 
-    //! [Adding the interface]
+    //! [Adding the interface1]
     rr_can_interface_t *iface = rr_init_interface(TUTORIAL_DEVICE);
-    //! [Adding the interface]
+    //! [Adding the interface 1]
     
-    //! [Adding the servo]
+    //! [Adding the servo1]
     rr_servo_t *servo = rr_init_servo(iface, TUTORIAL_SERVO_0_ID);
-    //! [Adding the servo]
+    //! [Adding the servo1]
 
     API_DEBUG("========== Tutorial of the %s ==========\n", "controlling one servo");
 
-    //! [Clear points all]
+    //! [Clear points all1]
     rr_clear_points_all(servo);
-    //! [Clear points all]
+    //! [Clear points all1]
     API_DEBUG("Appending points\n");
     //! [Add motion point first]
     int status = rr_add_motion_point(servo, 100.0, 0.0, 6000);
@@ -78,11 +78,11 @@ int main(int argc, char *argv[])
         return 1;
     }
     //! [Add motion point second]
-    //! [Start motion]
+    //! [Start motion1]
     rr_start_motion(iface, 0);
-    //! [Start motion]
-    //! [Sleep]
+    //! [Start motion1]
+    //! [Sleep1]
     rr_sleep_ms(14000); // wait till the movement ends
-    //! [Sleep]
+    //! [Sleep1]
     //! [cccode 1]
 }
