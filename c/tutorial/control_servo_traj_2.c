99d197dffccac083c5aea7e1180e21ad30e3ec27
Alesia Bulanok <alesia.bulanok@rozum.com>
2018-07-16 15:23:05 +0300

    Update control_servo_traj_2.c

--- c/tutorial/control_servo_traj_2.c
+++ c/tutorial/control_servo_traj_2.c
@@ -14,6 +14,8 @@
  * The tutorial describes how to set up motion trajectories for two servos and to execute them simultaneously. In this example, each motion trajectory comprises two PVT (position-velocity-time) points:
  * <ul><li>one PVT commanding servos to move to the position of 100 degrees in 6,000 milliseconds</li>
  * <li>one PVT commanding servos to move to the position of -100 degrees in 6,000 milliseconds</li></ul>
+ * 
+ * <p><b>Note: </b>Before setting up PVT points, make sure to change the default CAN ID of at least one of the servos (see the \ref tutor_c_changeID1 tutorial).</p>
 
  * 1. Initialize the interface.
  * \snippet control_servo_traj_2.c Adding the interface2
