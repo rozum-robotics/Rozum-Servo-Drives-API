0f15723e6f258ca8faacc13994abcc63e9354f07
ilya.grozov <ilya.grozov@rozum.com>
2018-07-28 20:42:37 +0300

    [+]: reading parameters and fixes in return values

--- python/rozum/servo.py
+++ python/rozum/servo.py
@@ -2,9 +2,91 @@ from rozum.util import Singleton
 from ctypes import *
 import time
 import os
+from enum import IntEnum
 # TODO Switching servo working states: rr_setup_nmt_callback, rr_describe_nmt
 # TODO Check statuses and logging
 # TODO add invoke_time_calculation
+# TODO write doc strings
+# TODO tests
+# TODO constants as enums and modify _api functions so that they return enums
+
+(RET_OK,
+ RET_ERROR,
+ RET_BAD_INSTANCE,
+ RET_BUSY,
+ RET_WRONG_TRAJ,
+ RET_LOCKED,
+ RET_STOPPED,
+ RET_TIMEOUT,
+ RET_ZERO_SIZE,
+ RET_SIZE_MISMATCH,
+ RET_WRONG_ARG) = map(c_int, range(11))
+
+(APP_PARAM_NULL,
+ APP_PARAM_POSITION,
+ APP_PARAM_VELOCITY,
+ APP_PARAM_POSITION_ROTOR,
+ APP_PARAM_VELOCITY_ROTOR,
+ APP_PARAM_POSITION_GEAR_360,
+ APP_PARAM_POSITION_GEAR_EMULATED,
+ APP_PARAM_CURRENT_INPUT,
+ APP_PARAM_CURRENT_OUTPUT,
+ APP_PARAM_VOLTAGE_INPUT,
+ APP_PARAM_VOLTAGE_OUTPUT,
+ APP_PARAM_CURRENT_PHASE,
+ APP_PARAM_TEMPERATURE_ACTUATOR,
+ APP_PARAM_TEMPERATURE_ELECTRONICS,
+ APP_PARAM_TORQUE,
+ APP_PARAM_ACCELERATION,
+ APP_PARAM_ACCELERATION_ROTOR,
+ APP_PARAM_CURRENT_PHASE_1,
+ APP_PARAM_CURRENT_PHASE_2,
+ APP_PARAM_CURRENT_PHASE_3,
+ APP_PARAM_CURRENT_RAW,
+ APP_PARAM_CURRENT_RAW_2,
+ APP_PARAM_CURRENT_RAW_3,
+ APP_PARAM_ENCODER_MASTER_TRACK,
+ APP_PARAM_ENCODER_NONIUS_TRACK,
+ APP_PARAM_ENCODER_MOTOR_MASTER_TRACK,
+ APP_PARAM_ENCODER_MOTOR_NONIUS_TRACK,
+ APP_PARAM_TORQUE_ELECTRIC_CALC,
+ APP_PARAM_CONTROLLER_VELOCITY_ERROR,
+ APP_PARAM_CONTROLLER_VELOCITY_SETPOINT,
+ APP_PARAM_CONTROLLER_VELOCITY_FEEDBACK,
+ APP_PARAM_CONTROLLER_VELOCITY_OUTPUT,
+ APP_PARAM_CONTROLLER_POSITION_ERROR,
+ APP_PARAM_CONTROLLER_POSITION_SETPOINT,
+ APP_PARAM_CONTROLLER_POSITION_FEEDBACK,
+ APP_PARAM_CONTROLLER_POSITION_OUTPUT,
+ APP_PARAM_CONTROL_MODE,
+ APP_PARAM_FOC_ANGLE,
+ APP_PARAM_FOC_IA,
+ APP_PARAM_FOC_IB,
+ APP_PARAM_FOC_IQ_SET,
+ APP_PARAM_FOC_ID_SET,
+ APP_PARAM_FOC_IQ,
+ APP_PARAM_FOC_ID,
+ APP_PARAM_FOC_IQ_ERROR,
+ APP_PARAM_FOC_ID_ERROR,
+ APP_PARAM_FOC_UQ,
+ APP_PARAM_FOC_UD,
+ APP_PARAM_FOC_UA,
+ APP_PARAM_FOC_UB,
+ APP_PARAM_FOC_U1,
+ APP_PARAM_FOC_U2,
+ APP_PARAM_FOC_U3,
+ APP_PARAM_FOC_PWM1,
+ APP_PARAM_FOC_PWM2,
+ APP_PARAM_FOC_PWM3,
+ APP_PARAM_FOC_TIMER_TOP,
+ APP_PARAM_DUTY,
+ APP_PARAM_SIZE) = map(c_int, range(59))
+
+
+class CtypesEnum(IntEnum):
+    @classmethod
+    def from_param(cls, obj):
+        return int(obj)
 
 
 class Servo(object):
@@ -12,6 +94,22 @@ class Servo(object):
         self._api = library_api
         self._servo = servo_interface
         self._identifier = identifier
+
+    def param_cache_update(self):
+        self._api.rr_param_cache_update(self._servo)
+
+    def param_cache_setup_entry(self, param: c_int, enabled: bool):
+        self._api.rr_param_cache_setup_entry(self._servo, param, c_bool(enabled))
+
+    def read_parameter(self, param: c_int):
+        value = c_float()
+        self._api.rr_read_parameter(self._servo, param, byref(value))
+        return value.value
+
+    def read_cached_parameter(self, param: c_int):
+        value = c_float()
+        self._api.rr_read_cached_parameter(self._servo, param, byref(value))
+        return value.value
 
     def set_zero_position(self, position_deg: float):
         self._api.rr_set_zero_position(self._api, c_float(position_deg))
@@ -22,7 +120,7 @@ class Servo(object):
     def get_max_velocity(self):
         velocity = c_float()
         self._api.rr_get_max_velocity(self._servo, byref(velocity))
-        return velocity
+        return velocity.value
 
     def set_max_velocity(self, max_velocity_deg_per_sec: float):
         self._api.rr_set_max_velocity(self._servo, c_float(max_velocity_deg_per_sec))
@@ -40,12 +138,12 @@ class Servo(object):
     def get_points_size(self):
         size = c_uint32(0)
         self._api.rr_get_points_size(self._servo, byref(size))
-        return size
+        return size.value
 
     def get_points_free_space(self):
         size = c_uint32(0)
         self._api.rr_get_points_free_space(self._servo, byref(size))
-        return size
+        return size.value
 
     def stop_and_release(self):
         self._api.rr_stop_and_release(self._servo)
@@ -181,6 +279,10 @@ if __name__ == '__main__':
     api.init_interface("/dev/serial/by-id/usb-Rozum_Robotics_USB-CAN_Interface_301-if00")
     t_servo = api.init_servo(64)
     time.sleep(1)
-    print(t_servo.get_points_size())
+    t_servo.set_current(1)
+    time.sleep(2)
+    print(t_servo.read_parameter(APP_PARAM_DUTY))
     print(t_servo.get_points_free_space())
+    time.sleep(2)
+    t_servo.set_current(0)
     time.sleep(1)
