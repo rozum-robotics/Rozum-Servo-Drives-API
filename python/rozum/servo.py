d932a126dac0e8a0172ec85476c71d21f25e806c
ilya.grozov <ilya.grozov@rozum.com>
2018-08-06 21:26:57 +0300

    [-]: removed old logging

--- python/rozum/servo.py
+++ python/rozum/servo.py
@@ -2,7 +2,6 @@ import time
 import os
 import logging
 from functools import wraps
-from enum import IntEnum
 from rozum.util import Singleton
 from rozum.constants import *
 
@@ -12,29 +11,6 @@ logger.addHandler(logging.NullHandler())
 # TODO normal logging
 # TODO write doc strings
 # TODO tests
-# TODO constants as enums and modify _api functions so that they return enums
-
-
-class CtypesEnum(IntEnum):  # for future use
-    @classmethod
-    def from_param(cls, obj):
-        return int(obj)
-
-
-def _log_func_status(func, status):
-    message = "Call {} returned {}".format(func.__name__, RET_STATUS_MESSAGE[status])
-    if status == RET_OK.value:
-        logger.info(message)
-    else:
-        logger.error(message)
-
-
-def ret_status_t(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        status = func(*args, **kwargs)
-        _log_func_status(func, status)
-    return wrapper
 
 
 class Servo(object):
@@ -47,67 +23,54 @@ class Servo(object):
     def interface(self):
         return self._servo
 
-    @ret_status_t
     def param_cache_update(self):
         return self._api.rr_param_cache_update(self._servo)
 
-    @ret_status_t
     def param_cache_setup_entry(self, param: c_int, enabled: bool):
         return self._api.rr_param_cache_setup_entry(self._servo, param, c_bool(enabled))
 
     def read_parameter(self, param: c_int):
         value = c_float()
         status = self._api.rr_read_parameter(self._servo, param, byref(value))
-        _log_func_status(self.read_parameter, status)
         return value.value
 
     def read_cached_parameter(self, param: c_int):
         value = c_float()
         status = self._api.rr_read_cached_parameter(self._servo, param, byref(value))
-        _log_func_status(self.read_cached_parameter, status)
         return value.value
 
-    @ret_status_t
     def set_zero_position(self, position_deg: float):
         return self._api.rr_set_zero_position(self._api, c_float(position_deg))
 
-    @ret_status_t
     def set_zero_position_and_save(self, position_deg: float):
         return self._api.rr_set_zero_position_and_save(self._servo, c_float(position_deg))
 
     def get_max_velocity(self):
         velocity = c_float()
         status = self._api.rr_get_max_velocity(self._servo, byref(velocity))
-        _log_func_status(self.get_max_velocity, status)
         return velocity.value
 
-    @ret_status_t
     def set_max_velocity(self, max_velocity_deg_per_sec: float):
         return self._api.rr_set_max_velocity(self._servo, c_float(max_velocity_deg_per_sec))
 
-    @ret_status_t
     def add_motion_point(self, position_deg: float, velocity_deg_per_sec: float, time_ms: int):
         return self._api.rr_add_motion_point(self._servo,
                                              c_float(position_deg), c_float(velocity_deg_per_sec), c_uint32(time_ms))
 
-    @ret_status_t
     def clear_points_all(self):
         return self._api.rr_clear_points_all(self._servo)
 
-    @ret_status_t
     def clear_points(self, num_to_clear: int):
         return self._api.rr_clear_points(self._servo, c_uint32(num_to_clear))
 
     def get_points_size(self):
         size = c_uint32(0)
         status = self._api.rr_get_points_size(self._servo, byref(size))
-        _log_func_status(self.get_points_size, status)
         return size.value
 
     def get_points_free_space(self):
         size = c_uint32(0)
         status = self._api.rr_get_points_free_space(self._servo, byref(size))
-        _log_func_status(self.get_points_free_space, status)
         return size.value
 
     def invoke_time_calculation(self,
@@ -124,58 +87,45 @@ class Servo(object):
                                                       byref(calculated_time))
         return calculated_time.value
 
-    @ret_status_t
     def stop_and_release(self):
         return self._api.rr_stop_and_release(self._servo)
 
-    @ret_status_t
     def stop_and_freeze(self):
         return self._api.rr_stop_and_freeze(self._servo)
 
-    @ret_status_t
     def set_current(self, current_a: float):
         return self._api.rr_set_current(self._servo, c_float(current_a))
 
-    @ret_status_t
     def set_velocity(self, velocity_deg_per_sec: float):
         return self._api.rr_set_velocity(self._servo, c_float(velocity_deg_per_sec))
 
-    @ret_status_t
     def set_position(self, position_deg: float):
         return self._api.rr_set_position(self._servo, c_float(position_deg))
 
-    @ret_status_t
     def set_velocity_with_limits(self, velocity_deg_per_sec: float, current_a: float):
         return self._api.rr_set_velocity_with_limits(self._servo, c_float(velocity_deg_per_sec), c_float(current_a))
 
-    @ret_status_t
     def set_position_with_limits(self, position_deg: float, velocity_deg_per_sec: float, current_a: float):
         return self._api.rr_set_position_with_limits(self._servo,
                                                      c_float(position_deg),
                                                      c_float(velocity_deg_per_sec),
                                                      c_float(current_a))
 
-    @ret_status_t
     def set_duty(self, duty_percent: float):
         return self._api.rr_set_duty(self._servo, c_float(duty_percent))
 
-    @ret_status_t
     def reboot(self):
         return self._api.rr_servo_reboot(self._servo)
 
-    @ret_status_t
     def reset_communication(self):
         return self._api.rr_servo_reset_communication(self._servo)
 
-    @ret_status_t
     def set_state_operational(self):
         return self._api.rr_servo_set_state_operational(self._servo)
 
-    @ret_status_t
     def set_state_pre_operational(self):
         return self._api.rr_servo_set_state_pre_operational(self._servo)
 
-    @ret_status_t
     def set_state_stopped(self):
         return self._api.rr_servo_set_state_stopped(self._servo)
 
@@ -185,7 +135,6 @@ class Servo(object):
         status = self._api.rr_read_error_status(self._servo, byref(error_count), byref(error_array))
         return error_count.value, error_array
 
-    @ret_status_t
     def deinit_servo(self):
         return self._api.rr_deinit_servo(byref(c_void_p(self._servo)))
 
@@ -202,7 +151,6 @@ class Interface(object):
         self._servos = {}
         time.sleep(0.5)  # for interface initialization
 
-    @ret_status_t
     def start_motion(self, timestamp_ms: int):
         return self._api.rr_start_motion(self._interface, c_uint32(timestamp_ms))
 
@@ -219,31 +167,24 @@ class Interface(object):
     def change_id_and_save(self, old_id: int, new_can_id: int):
         servo_interface = self.init_servo(old_id).interface
         status = self._api.rr_change_id_and_save(self._interface, servo_interface, c_uint8(new_can_id))
-        _log_func_status(self.change_id_and_save, status)
-        self._servos[new_can_id] = Servo(self._api, servo_interface, new_can_id)
         del self._servos[old_id]
+        return status
 
-    @ret_status_t
     def net_reboot(self):
         return self._api.rr_net_reboot(self._interface)
 
-    @ret_status_t
     def net_reset_communication(self):
         return self._api.rr_net_reset_communication(self._interface)
 
-    @ret_status_t
     def net_set_state_operational(self):
         return self._api.rr_net_set_state_operational(self._interface)
 
-    @ret_status_t
     def net_set_state_pre_operational(self):
         return self._api.rr_net_set_state_pre_operational(self._interface)
 
-    @ret_status_t
     def net_set_state_stopped(self):
         return self._api.rr_net_set_state_stopped(self._interface)
 
-    @ret_status_t
     def deinit_interface(self):
         for servo in self._servos.values():
             if servo is not None:
@@ -260,7 +201,7 @@ class ServoApi(object, metaclass=Singleton):
 
     def _check_library_loaded(self):
         if self._api is None:
-            raise AttributeError("Library not loaded. Consider call api.load_library(path_to_library) first.")
+            raise AttributeError("Library not loaded. Call api.load_library(path_to_library) first.")
 
     def _check_interface_initialized(self):
         if self._interface is None:
