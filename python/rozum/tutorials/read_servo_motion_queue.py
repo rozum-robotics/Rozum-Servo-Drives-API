a9fba2c151d84b729bcacc7725efc9eb5f781a08
ilya.grozov <ilya.grozov@rozum.com>
2018-08-06 19:54:47 +0300

    [+]: read_servo_motion_queue

---
+++ python/rozum/tutorials/read_servo_motion_queue.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+
+from rozum.servo import ServoApi
+from rozum.tutorials import INTERFACE_NAME, SERVO_1_ID
+
+logging.basicConfig()
+logger = logging.getLogger(os.path.basename(__file__))
+logger.setLevel(logging.INFO)
+
+if __name__ == '__main__':
+    logger.info("Initializing ServoApi")
+    api = ServoApi()
+
+    logger.info("Loading library")
+    api.load_library()
+
+    logger.info("Initializing interface {}".format(INTERFACE_NAME))
+    interface = api.init_interface(INTERFACE_NAME)
+
+    logger.info("Initializing servo id {}".format(SERVO_1_ID))
+    servo = api.init_servo(SERVO_1_ID)
+
+    logger.info("Clearing points")
+    servo.clear_points_all()
+
+    points_size = servo.get_points_size()
+    logger.info("Points in queue before: {}".format(points_size))
+
+    free_points = servo.get_points_free_space()
+    logger.info("Points queue free space before: {}".format(free_points))
+
+    logger.info("Appending points")
+    servo.add_motion_point(0.0, 0.0, 10000000)
+    servo.add_motion_point(0.0, 0.0, 10000000)
+
+    points_size = servo.get_points_size()
+    logger.info("Points in queue after: {}".format(points_size))
+
+    free_points = servo.get_points_free_space()
+    logger.info("Points queue free space after: {}".format(free_points))
+
+    logger.info("Clearing points")
+    servo.clear_points_all()
