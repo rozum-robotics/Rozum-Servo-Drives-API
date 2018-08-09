ff86a339a632fd2ddcd535940dc5909c9a8176ce
ilya.grozov <ilya.grozov@rozum.com>
2018-08-09 14:43:03 +0300

    [+]: some docstrings

--- python/rozum/servo.py
+++ python/rozum/servo.py
@@ -139,10 +139,33 @@ class Servo(object):
         return error_count.value, error_array
 
     def deinit_servo(self):
+        """The function deinitializes the servo, clearing all data associated with the servo descriptor.
+
+        Automatically called on interface deinitialization.
+
+        :return: Status code: int
+        """
         return self._api.rr_deinit_servo(byref(self._servo))
 
     def _write_raw_sdo(self, idx: c_uint16, sidx: c_uint8, data: c_void_p, sz: c_int, retry: c_int, tout: c_int):
-        """Use on your own risk"""
+        """The function performs an arbitrary SDO write request to servo.
+
+        Use on your own risk.
+
+        :param idx: c_uint16:
+            Index of SDO object
+        :param sidx: c_uint8:
+            Subindex
+        :param data: c_void_p:
+            Data to write to
+        :param sz: c_int:
+            Size of data in bytes
+        :param retry: c_int:
+            Number of reties (if communication error occured during request)
+        :param tout: c_int:
+            Request timeout in milliseconds
+        :return: status: int
+        """
         return self._api.rr_write_raw_sdo(self._servo, idx, sidx, data, sz, retry, tout)
 
 
@@ -164,6 +187,7 @@ class Interface(object):
         return self._api.rr_start_motion(self._interface, c_uint32(timestamp_ms))
 
     def init_servo(self, identifier) -> Servo:
+        """See docstring of ServoApi.init_servo()"""
         if identifier not in self._servos:
             servo_interface = c_void_p(self._api.rr_init_servo(self._interface, c_byte(identifier)))
             if servo_interface is None:
@@ -174,6 +198,22 @@ class Interface(object):
         return self._servos[identifier]
 
     def change_id_and_save(self, old_id: int, new_can_id: int):
+        """The function enables changing the default CAN identifier (ID) of the specified servo to avoid collisions on
+        a bus line. **Important!** Each servo connected to a CAN bus must have **a unique ID**.
+
+        When called, the function resets CAN communication for the specified servo, checks that Heartbeats are generated
+        for the new ID, and saves the new CAN ID to the EEPROM memory of the servo.
+
+        **Note:** The EEPROM memory limit is 1,000 write cycles. Therefore, it is advisable to use the function with
+        discretion.
+
+        :param old_id: int:
+            Old CAN ID.
+        :param new_can_id: int:
+            New CAN ID. You can set any value within the range from 1 to 127, only make sure no other servo has the same
+            ID.
+        :return: status : int
+        """
         servo_interface = self.init_servo(old_id).interface
         status = self._api.rr_change_id_and_save(self._interface, servo_interface, c_uint8(new_can_id))
         del self._servos[old_id]
@@ -195,6 +235,14 @@ class Interface(object):
         return self._api.rr_net_set_state_stopped(self._interface)
 
     def deinit_interface(self):
+        """The function closes the COM port where the corresponding CAN-USB dongle is connected, clearing all data
+        associated with the interface descriptor.
+
+        Automatically called when ServoApi is deinitializing. In addition performs deinitialization on all registered
+        servos.
+
+        :return: None
+        """
         for servo in self._servos.values():
             if servo is not None:
                 servo.deinit_servo()
@@ -228,6 +276,14 @@ class ServoApi(object, metaclass=Singleton):
         return self._interface
 
     def load_library(self, library_path: str = None):
+        """The function is the first to call to be able to work with the user API.
+
+        It tries to load library with respect to given path. If path is not provided, it tries to find library in
+        current directory.
+
+        :param library_path: str or None
+        :return: None
+        """
         if library_path is None and self._api is None:
             self._api = CDLL(os.path.join(os.path.dirname(__file__), "libservo_api.so"))
         else:
@@ -236,16 +292,51 @@ class ServoApi(object, metaclass=Singleton):
             if self._api is None:
                 self._api = CDLL(library_path)
 
-    def init_interface(self, interface_name: str):
+    def init_interface(self, interface_name: str) -> Interface:
+        """The function is the second to call to be able to work with the user API.
+
+        It opens the COM port where the corresponding CAN-USB dongle is connected, enabling communication between the
+        user program and the servo motors on the respective CAN bus.
+
+        Interface name string examples:
+            * Linux: "/dev/ttyACM0" **or** "/dev/serial/by-id/usb-Rozum_Robotics_USB-CAN_Interface_301-if00"
+            * MacOS: "/dev/cu.modem301"
+            * Windows (Cygwin): "/dev/ttyS0"
+        *Note: last numbers in "/dev/.." strings may differ on your machine.*
+
+        :param interface_name: str
+        :return: Interface instance
+        """
         self._check_library_loaded()
         if self._interface is None:
             self._interface = Interface(self._api, interface_name)
         return self._interface
 
     def init_servo(self, identifier: int) -> Servo:
+        """_init_servo_label:
+        The function determines whether the servo motor with the specified ID is connected to the specified interface.
+
+         It waits for 2 seconds to receive a Heartbeat message from the servo. When the message arrives within the
+         interval, the servo is identified as successfully connected.
+
+        :param identifier: int:
+            Unique identifier of the servo in the specified interface. The available value range is from 0 to 127.
+        :return: Servo instance
+        """
         return self.interface.init_servo(identifier)
 
     def sleep_ms(self, ms: int):
+        """The function sets an idle period for the user program (e.g., to wait till a servo executes a motion trajectory).
+
+        Until the period expires, the user program will not execute any further operations.
+        However, the network management, CAN communication, emergency, and Heartbeat functions remain available.
+        Note:The user can also call system-specific sleep functions directly.
+        However, using this sleep function is preferable to ensure compatibility with subsequent API library versions.
+
+        :param ms: int:
+            Idle period (in milleseconds)
+        :return: None
+        """
         self._api.rr_sleep_ms(c_int(ms))
 
     def describe_emcy_bit(self, bit: c_uint8):
