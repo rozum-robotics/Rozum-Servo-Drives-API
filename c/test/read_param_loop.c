9536c137e3829c7446822886842b2b528e987c41
katkov <kmxm@tut.by>
2018-11-03 04:05:47 +0300

    [*]: read loop test -> test folder

--- c/test/read_param_loop.c
+++ c/test/read_param_loop.c
@@ -10,22 +10,30 @@
 #include "tutorial.h"
 
 #include <sys/time.h>
+#include <stdlib.h>
 
 #define TIME_DELTA_MS(x, y) ((x.tv_sec - y.tv_sec) * 1000 + (x.tv_usec - y.tv_usec) / 1000)
 #define TIME_DELTA_US(x, y) ((x.tv_sec - y.tv_sec) * 1000000 + (x.tv_usec - y.tv_usec) )
  
 int main(int argc, char *argv[])
 {
-    rr_can_interface_t *iface = rr_init_interface(TUTORIAL_DEVICE);
-    rr_servo_t *servo = rr_init_servo(iface, TUTORIAL_SERVO_0_ID);
+	if(argc < 3)
+	{
+		printf("Usage: %s port id\n", argv[0]);
+		exit(1);
+	}
+
+    rr_can_interface_t *iface = rr_init_interface(argv[1]);
+    rr_servo_t *servo = rr_init_servo(iface, strtol(argv[2], 0, 0));
     float value;
 	struct timeval tprev, tnow;
 
-	for(int i = 0; i < 1000; i++)
+	while(1)
 	{
-		//gettimeofday(&tprev, NULL);
+		gettimeofday(&tprev, NULL);
 		rr_read_parameter(servo, APP_PARAM_POSITION_ROTOR, &value);
-		//gettimeofday(&tnow, NULL);
-		//fprintf(stderr, "%d: time_us %ld, value %f\n", i, TIME_DELTA_US(tnow, tprev), value);
+		gettimeofday(&tnow, NULL);
+		fprintf(stderr, "time_us %ld, value %f\n", TIME_DELTA_US(tnow, tprev), value);
 	}
+
 }
